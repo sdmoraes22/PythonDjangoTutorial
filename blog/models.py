766af63ab3ed8e40a0b54bf09082d9c2ffe8b059
Cristiano Moraes <cmoraes.ti@gmail.com>
2021-04-04 20:05:28 -0300

    Blog migration added for Posts

--- blog/models.py
+++ blog/models.py
@@ -5,4 +5,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     slug = models.CharField(max_length=255, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
